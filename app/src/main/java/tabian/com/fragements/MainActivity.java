4e0ead4d03fc7fba084e836e8e94a151099d1b4e
Hang Barnabas <hang.barnabas@telekom.hu>
2017-09-01 21:26:11 +0200

    Initial commit

---
+++ app/src/main/java/tabian/com/fragements/MainActivity.java
@@ -0,0 +1,48 @@
+package tabian.com.fragements;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
+    private ViewPager mViewPager;
+
+    public String whatIsThis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Started.");
+
+        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.containter);
+        //setup the pager
+        setupViewPager(mViewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment1(), "Fragment1");
+        adapter.addFragment(new Fragment2(), "Fragment2");
+        adapter.addFragment(new Fragment3(), "Fragment3");
+        viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber){
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
+    public void lol(View v) {
+        Toast.makeText(this, "HACKERMAN WAS HERE 1", Toast.LENGTH_SHORT).show();
+    }
+}
